69a1fc030ef8464ef3a7c36df0210eecb51078f8
SAMUK-HP\Usuário <johnvictorio3@gmail.com>
2018-02-23 13:01:55 -0300

    _Testando commit_

--- app/User.php
+++ app/User.php
@@ -11,8 +11,9 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
+     * 
      * @var array
+     *
      */
     protected $fillable = [
         'name', 'email', 'password',
