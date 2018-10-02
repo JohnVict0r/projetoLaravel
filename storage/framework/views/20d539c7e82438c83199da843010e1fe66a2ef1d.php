c3de9822ce9cd64cfd01cd9028e4f27c8e8cf2de
jilcimar <jilcimar.fernandes0267@gmail.com>
2018-10-01 23:24:44 -0300

    git ignore funcionando

--- storage/framework/views/20d539c7e82438c83199da843010e1fe66a2ef1d.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', 'Page Not Found'); ?>
-
-<?php $__env->startSection('message', 'Desculpe-nos, a página que está buscando não existe mais!'); ?>
-
-<?php echo $__env->make('errors::layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
